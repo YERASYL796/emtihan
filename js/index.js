58d09f8ec4136c9726a9a1ab60887a13397a3917
erasyl <agabekerasyl2@gmail.com>
2024-11-11 18:00:33 +0500

    exam4

---
+++ js/index.js
@@ -0,0 +1,89 @@
+
+//1-ЫНШЫ ТАПСЫРМА 
+
+// let age = +prompt("1-den 12-ge dein san engiziniz");
+
+// if(age >=3 && age <= 5){
+//     alert("ai "+age+"mausim:spring")
+// }
+// else if(age>=6 && age <=8){
+//     alert("ai "+age+"mausim: summer")
+// }
+// else if (age>=9  && age<=11){
+//     alert("ai "+age+"mausim:autumn")
+// }
+// else if (age==12 || age==2 || age==1){
+//     alert("ai "+age+"mausim:winter")
+// }
+// else{
+//     alert("bundai ailar joh")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 2-ЫНШЫ ТАПСЫРМА
+
+
+// let a = parseFloat(prompt("birinshi sandi engiziniz:"));
+// let b = parseFloat(prompt("ekenshi sandi engiz:"));
+
+// if (a > b) {
+//     alert("a sany b sanynan ulken");
+// } else if (a < b) {
+//     alert("a sany b sanynan kishi");
+// } else {
+//     alert("sandar ten");
+// }
+
+// let average = (a + b) / 2;
+// alert("ortasha mani ten: " + average);
+
+
+
+
+
+
+
+//3-ЫНШЫ ТАПСЫРМА
+
+let a = parseFloat(prompt("birinshi sandy engiziniz:"));
+let b = parseFloat(prompt("ekinshi sandy engiziniz"));
+let c = parseFloat(prompt("ushinshi sandi engiziniz"));
+
+if( a > b){
+    alert("a sany b sanynan ulken");
+}else if ( a < b){
+    alert("a sany b sanynan kishi");
+}else {
+    alert("En ulken jane e  kishi sannyn airmasy:")
+}
+let average = ( a - b);
+alert("En ulken jane e  kishi sannyn airmasy:" + average);
+
+
+
+
+
+
+
+
